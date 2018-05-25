805698fc96663baba3eb9b9b6f3586889d5868e3
Marijn <marijn.j93@gmail.com>
2018-05-25 15:55:54 +0200

    Checksum controle voordat het originele bestand verwijdert wordt.

--- Linux week 2 opdracht - BASH script.sh
+++ Linux week 2 opdracht - BASH script.sh
@@ -15,8 +15,7 @@ echo "Bijvoorbeeld: /home/marijn/Pictures_sorted"
 read -p "Folder: " newdirectory
 echo
 
-echo "Voer een maandnummer in om foto’s in de geselecteerde folder naar een (nieuw te creëren) folder te verplaatsen "
-echo "die ouder zijn dan de gekozen optie."
+echo "Voer een maandnummer in om foto’s in de geselecteerde folder naar een (nieuw te creëren) folder te verplaatsen die ouder zijn dan de gekozen optie."
 echo "Bijvoorbeeld: 09"
 read -p "Maandnummer: " monthnumber
 echo
@@ -32,10 +31,16 @@ do
 	filenameofthemonth=$(date -r $f +%B)
 	if [ $filemonth \< $monthnumber ]; then
 		echo "Het bestand is ouder dan de ingevoerde maand en zal verplaatst worden."
-		echo
 		fullpath=$newdirectory"/"$fileyear"/"$filenameofthemonth
 		mkdir -p $fullpath
-		mv "$f" $fullpath
+		checksum=($(md5sum $f))
+		cp "$f" $fullpath
+		newchecksum=($(md5sum $fullpath"/"$f))
+		if [ $checksum == $newchecksum ]; then
+			echo "Checksum komt overeen dus het origineel wordt verwijdert."
+			rm $f
+		fi
+		echo
 	fi
 
 	if [ $filemonth \= $monthnumber ]; then
@@ -48,6 +53,6 @@ do
 	fi
 done
 
-# Laat de inhoud van de nieuwe folder zien.
-echo "De inhoud van de nieuwe folder is:"
+# Geef een overzicht van de gesorteerde bestanden.
+echo "Het overzicht van de gesorteerde bestanden:"
 tree "$newdirectory"
