9df86060ed17616758dbc2bce0cca7d4e1c0b7ac
Marijn <marijn.j93@gmail.com>
2018-05-25 15:01:24 +0200

    Verbetering van het script.
    De bestanden worden nu ook per jaar ingedeeld.

--- Linux week 2 opdracht - BASH script.sh
+++ Linux week 2 opdracht - BASH script.sh
@@ -6,19 +6,19 @@ echo "Het script start nu."
 echo
 
 echo "Voer de volledige locatie in waar de foto's staan die verplaatst moeten worden."
-echo "Bijvoorbeeld: /home/marijn/f"
+echo "Bijvoorbeeld: /home/marijn/Pictures"
 read -p "Directory: " directory
+echo
+
+echo "Voer de locatie van de folder in, waar de foto's verplaatst naar moeten worden als die ouder zijn dan de gekozen optie."
+echo "Bijvoorbeeld: /home/marijn/Pictures_sorted"
+read -p "Folder: " newdirectory
 echo
 
 echo "Voer een maandnummer in om foto’s in de geselecteerde folder naar een (nieuw te creëren) folder te verplaatsen "
 echo "die ouder zijn dan de gekozen optie."
 echo "Bijvoorbeeld: 09"
 read -p "Maandnummer: " monthnumber
-echo
-
-echo "Voer de locatie van de folder in, waar de foto's verplaatst naar moeten worden als die ouder zijn dan de gekozen optie."
-echo "Bijvoorbeeld: /home/marijn/Pictures/hierin"
-read -p "Folder: " newdirectory
 echo
 
 cd $directory
@@ -28,12 +28,14 @@ for f in $search
 do
 	echo "Bestandsnaam: $f"
 	filemonth=$(date -r $f +%m)
-
+	fileyear=$(date -r $f +%Y)
+	filenameofthemonth=$(date -r $f +%B)
 	if [ $filemonth \< $monthnumber ]; then
 		echo "Het bestand is ouder dan de ingevoerde maand en zal verplaatst worden."
 		echo
-		mkdir -p "$newdirectory"
-		cp "$f" "$newdirectory"
+		fullpath=$newdirectory"/"$fileyear"/"$filenameofthemonth
+		mkdir -p $fullpath
+		mv "$f" $fullpath
 	fi
 
 	if [ $filemonth \= $monthnumber ]; then
@@ -45,5 +47,7 @@ do
 		echo
 	fi
 done
+
+# Laat de inhoud van de niewe folder zien.
 echo "De inhoud van de nieuwe folder is:"
-ls -ha "$newdirectory"
+tree "$newdirectory"
